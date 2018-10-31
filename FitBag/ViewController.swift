24d4eae7fb6fc1d4b8e5449780706acbf25b3d3f
Bianca Campos <biancancampos@gmail.com>
2018-10-31 11:25:12 -0700

    Added the first version of unit test

---
+++ FitBag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitBag
+//
+//  Created by Bianca Campos on 2018-10-25.
+//  Copyright © 2018 Bianca Campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ FitBagTests/FitBagTests.swift
@@ -0,0 +1,49 @@
+//
+//  FitBagTests.swift
+//  FitBagTests
+//
+//  Created by Bianca Campos on 2018-10-25.
+//  Copyright © 2018 Bianca Campos. All rights reserved.
+//
+
+import XCTest
+@testable import FitBag
+
+class FitBagTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testDatabase() {
+        // test connection with database
+    }
+
+    func testTableView(){
+        // test the data in tableView
+    }
+
+    func testSearchList(){
+        // test the search in the airline list
+    }
+    
+    func testTypeOfBaggage(){
+        // test the type of baggage
+    }
+    
+    func testMeasure(){
+        // test the connection with measure app
+    }
+
+    func testWebsite(){
+        // test the connection with website
+    }
+    
+    func testShare(){
+        // test the share functionality
+    }
+}
